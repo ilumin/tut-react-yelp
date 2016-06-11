c912ebaa5df420db8821c6c53b5144b76f869355
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-11 15:42:05 +0700

    UPDATE krama config refactor and add sourcemap loader plugin

--- karma.conf.js
+++ karma.conf.js
@@ -1,50 +1,39 @@
+var path = require('path');
 var webpackConfig = require('./webpack.config');
 
 module.exports = function(config) {
   config.set({
-
     basePath: '',
-
     frameworks: ['mocha', 'chai'],
+    files: [
+      'tests.webpack.js'
+    ],
+
+    preprocessors: {
+      'tests.webpack.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    },
 
     plugins: [
       'krama-mocha',
       'krama-chai',
       'krama-webpack',
       'karma-phantomjs-launcher',
-      'karma-spec-reporter'
+      'karma-spec-reporter',
+      'karma-sourcemap-loader'
     ],
-
-    webpack: webpackConfig,
-
-    webpackServer: {
-      noInfo: true
-    },
-
-    files: [
-      'tests.webpack.js'
-    ],
-
-    exclude: [
-    ],
-
-    preprocessors: {
-    },
 
     reporters: ['spec'],
-
     port: 9876,
-
     colors: true,
-
     logLevel: config.LOG_INFO,
-
     autoWatch: true,
-
     browsers: ['PhantomJS'],
-
     singleRun: false,
-
     concurrency: Infinity
   })
 }
