292a105d36479cd6afe47c829b5cf72a88a9322c
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-12 11:15:52 +0700

    ADD detail container

--- src/views/Main/routes.js
+++ src/views/Main/routes.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import {Route} from 'react-router'
 
+// main container
 import Container from './Container'
+
+// child container
 import Map from './Map/Map'
+import Detail from './Detail/Detail'
 
 export const makeMainRoutes = () => {
   console.log('Enter container routes');
   return (
     <Route path="/" component={Container}>
       <Route path="map" component={Map} />
+      <Route path="detail/:placeId" component={Detail} />
     </Route>
   )
 }
