22c9d28d52e6b713eac9a9fa535cab145d542cd7
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-12 08:58:40 +0700

    ADD simple router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'font-awesome/css/font-awesome.css'
 import './app.css'
+
+import {browserHistory, Router, Route} from 'react-router'
+
 import App from 'containers/App/App'
 
+const Home = React.createClass({
+  render: function() {
+    return (<div>Hello world</div>)
+  }
+})
+const routes = (
+  <Router>
+    <Route path="/" component={Home} />
+  </Router>
+)
+
 const mountNode = document.querySelector('#root');
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(
+  <App
+    history={browserHistory}
+    routes={routes}
+    />,
+  mountNode
+);
