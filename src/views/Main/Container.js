50181e29ae708acfd4612a3efdf611d2a2a590a1
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-12 09:49:39 +0700

    UPDATE main container set container state after finished search near by

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -21,7 +21,10 @@ export class Container extends React.Component {
 
     searchNearby(google, map, opts)
       .then((results, pagination) => {
-        // We got some results and a pagination object
+        this.setState({
+          places: results,
+          pagination
+        })
       })
       .catch((status, result) => {
         // There was an error
