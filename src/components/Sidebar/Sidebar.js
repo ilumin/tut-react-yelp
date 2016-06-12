5ac5093598c6cf4be1f6da3472ff7c96734683dc
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-12 10:35:03 +0700

    UPDATE sidebar component use listing components to list all places

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,17 +1,15 @@
 import React, { PropTypes as T } from 'react'
 import styles from './styles.module.css'
 
+import Listing from 'components/Listing/Listing'
+
 export class Sidebar extends React.Component {
   render() {
     return (
       <div className={styles.sidebar}>
         <div className={styles.heading}>
           <h1>{this.props.title}</h1>
-          <div className={styles.content}>
-            {this.props.places.map(place => {
-              return (<div key={place.id}>{place.name}</div>)
-            })}
-          </div>
+          <Listing places={this.props.places} />
         </div>
       </div>
     )
